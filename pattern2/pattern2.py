a7f4ff23c3b0ed54adf4392c6c39a8c39a544d4d
keshav <49027903+KESHAVBERG@users.noreply.github.com>
2021-09-05 10:08:03 +0530

    Add files via upload

---
+++ pattern2/pattern2.py
@@ -0,0 +1,15 @@
+import turtle as t
+
+colors = ['red', 'green','yellow', 'cyan']
+t.bgcolor('black')
+t.pensize(10)
+for x in range(0,2):
+    angle = x*100
+for i in range(0,100):
+    for b in range(0,2):
+        for c in range(len(colors)):
+            t.pencolor(colors[c])
+            t.forward(100+i*2)
+            t.right(angle+b)
+
+            
